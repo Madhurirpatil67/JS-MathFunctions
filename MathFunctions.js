cb353a801b278a3223efd7717c36bfa418cd2911
Madhurirpatil67 <madhurisankpal45@gmail.com>
2020-04-18 14:03:21 +0530

    MathFunctions

---
+++ MathFunctions.js
@@ -0,0 +1,49 @@
+var num=9;
+var fnum=10.6;
+var result=undefined;
+
+//sqrt
+result=Math.sqrt(num);
+console.log("sqrt:"+result);
+
+//pow
+result=Math.pow(num,3);
+console.log("pow :"+result);
+
+//random
+result=Math.random(num);
+console.log("random :"+result);
+
+//abs
+result=Math.abs(num);
+console.log("abs :"+result);
+
+//round
+result=Math.round(fnum);
+console.log("round :"+result);
+
+//floor
+result=Math.floor(fnum);
+console.log("floor :"+result);
+
+//ceil
+result=Math.ceil(fnum);
+console.log("ceil :"+result);
+
+//trunc
+result=Math.trunc(fnum);
+console.log("trunc :"+result);
+
+//min
+result=Math.min(8,6,2,5,7);
+console.log("min :"+result);
+
+//max
+result=Math.max(8,6,2,5,7);
+console.log("max :"+result);
+
+//log
+console.log("log :"+Math.log(num));
+
+//exp
+console.log("exp :"+Math.exp(num));
